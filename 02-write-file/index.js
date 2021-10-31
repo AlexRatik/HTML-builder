0c55a480dc02f4601d606dd8c528475e2f194764
AlexRatik <aleksandr-ratkevich@mail.ru>
2021-10-31 17:12:42 +0300

    feat: add 2nd task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,11 +2,31 @@ const path = require("path");
 const fs = require("fs");
 const process = require("process");
 const readline = require("readline");
+const { stdout, stdin } = require("process");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
+
 const out = fs.createWriteStream(path.join(__dirname, "text.txt"));
+
+// stdout.write("Input text for file please:\n", (err) => {
+//     if (err) console.log(err);
+// });
+
+// stdin.on("data", (data) => {
+//     if (data.toString().trim() === "exit") {
+//         stdout.write("Thanks, good luck=)");
+//         process.exit();
+//     } else {
+//         out.write(data.toString());
+//     }
+// });
+
+// process.on("SIGINT", () => {
+//     stdout.write("Thanks, good luck=)");
+//     process.exit();
+// });
 
 rl.question("Input text for file, please\n", function (text) {
     out.write(text + "\n");
